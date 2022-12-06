3106e9418ab5cac6b16a0bc321d5b1b7b9c5a118
Xavier van Heule <59011468+xvheule@users.noreply.github.com>
2022-12-06 09:42:37 +0000

    Add central access point

---
+++ stepper_control.py
@@ -0,0 +1,19 @@
+from motor.motor_control import MotorControl
+from motor.motor_interface import MotorInterface
+
+
+# Creates a new motor controller object
+# - port: a string specifying the COM port (e.g. 'COM3')
+# - time_out: an integer specifying the time in seconds to wait until no response is considered a time-out
+# - message_func: a function reference accepting a single String as parameter
+# - debug: a boolean, when True, all communication with the motor will be printed to the console
+def create_motor_controller(port, time_out, message_func, debug=False):
+    return MotorControl(port, time_out, message_func, debug)
+
+
+# Creates a new motor interface object
+# - port: a string specifying the COM port (e.g. 'COM3')
+# - value_func: a function reference accepting a single integer as parameter
+# - message_func: a function reference accepting a single String as parameter
+def create_motor_interface(port, value_func, message_func):
+    return MotorInterface(port, value_func, message_func)
